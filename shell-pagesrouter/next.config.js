f881685bac28d8753853bf1b2aae15d9ddb11968
reisblucas <lucasb.reis@hotmail.com>
2023-11-14 11:34:04 -0300

    chore(shell): add config module federation

--- shell-pagesrouter/next.config.js
+++ shell-pagesrouter/next.config.js
@@ -1,6 +1,27 @@
 /** @type {import('next').NextConfig} */
+const NextFederationPlugin = require('@module-federation/nextjs-mf')
+
+const remotes = (isServer) => {
+  const location = isServer ? 'ssr' : 'chunks'
+  return {
+    client: `client@http://localhost:3001/_next/static/${location}/remoteEntry.js`,
+    member: `member@http://localhost:3002/_next/static/${location}/remoteEntry.js`
+  }
+}
+
 const nextConfig = {
   reactStrictMode: true,
+  webpack(config, options) {
+    const { isServer } = options
+    config.plugins.push(
+      new NextFederationPlugin({
+        name: 'Shell-Layout',
+        filename: 'static/chunks/remoteEntry.js',
+        remotes: remotes(isServer)
+      })
+    )
+    return config
+  }
 }
 
 module.exports = nextConfig
